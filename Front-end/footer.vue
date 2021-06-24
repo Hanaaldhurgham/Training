a7b2b6bd3c0e9b50db6091575a1406da80e0c188
Hana <62803173+Hanaaldhurgham@users.noreply.github.com>
2021-06-24 16:43:36 +0300

    Add files via upload

---
+++ Front-end/footer.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <!-- Footer -->
+  <mdb-footer color="blue" class="font-small pt-4 mt-4">
+    <mdb-container class="footer">
+      <mdb-row>
+        <mdb-col sm="6">
+        
+         
+        </mdb-col>
+        <mdb-col sm="6">
+          
+      <a href="#">Contact us</a>
+   
+         
+         
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+    <div class="footer">
+      <mdb-container fluid>
+        &copy; 2020 Copyright:<li> <a href="https://www.MDBootstrap.com">  </a> </li>
+      </mdb-container>
+    </div>
+  </mdb-footer>
+  <!-- Footer -->
+</template>
+
+<style>
+ 
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color:rgb(43, 4, 25);
+  text-align: center;
+ 
+}
+
+</style>
---
+++ Front-end/home.vue
@@ -0,0 +1,19 @@
+vbase:<template>
+  <li class="w-nav__item" >
+         <router-link
+          to="/about"
+          class="nav-item nav-link">
+          About
+        </router-link>
+      </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
