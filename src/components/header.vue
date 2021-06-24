123cca451ba6e1d5c6f0335d19dc8a6d0a5e32de
Hanaaldhurgham <hanaa_dh@hotmail.com>
2021-06-24 16:58:17 +0300

    push it

---
+++ src/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <nav class="h">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <li class="w-nav__item" >
+         <router-link
+          to="/Home"
+          class="nav-item nav-link">
+          Home
+        </router-link>
+      </li>
+
+      <li class="w-nav__item" >
+         <router-link
+          to="/ViewT"
+          class="nav-item nav-link">
+          View
+        </router-link>
+      </li>
+</nav>
+ 
+</template>
+ 
+<script>
+   export default{
+ 
+   }
+</script>
+ 
+<style>
+.h{
+
+     display: block;
+ 
+  background-color: lightgrey;
+   display: block;
+ 
+   text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+word-spacing: 90px;
+   margin-right: 20px;
+}
+ 
+</style>
---
+++ src/components/table.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+      <table class="table table-bordered table-striped table-hover">
+     
+      
+        
+          <thead>
+            <th>
+                 <div class="table-th">
+                    <span class="lable">Student ID</span>
+                 </div>
+            </th>
+ 
+            <th>
+                 <div class="table-th">
+                    <span class="lable">Email</span>
+                 </div>
+            </th>
+ 
+            <th>
+                 <div class="table-th">
+                    <span class="lable">Student name</span>
+                 </div>
+            </th>
+ 
+            
+          </thead>
+ 
+          <tbody>
+            <tr v-for="user in users" :key="user.name">
+              <td>
+                <span>
+                  <span class="table-lable">{{ user.name }}</span>
+                </span>
+              </td>
+ 
+              <td>
+                <span>
+                  <span class="table-lable">{{ user.id }}</span>
+                </span>
+              </td>
+ 
+              
+            </tr>
+          </tbody>
+        
+ 
+ 
+</table>
+ 
+    </div>
+</template>
+
+<script>
+const axios = require('axios');
+    export default {
+      name: "tables",
+      data(){
+        return{
+          users:[
+
+
+         
+          ]
+ 
+          
+        };
+      },
+        mounted () {
+    axios
+      .get('http://localhost:3003/employees')
+      .then(response => (this.users = response.data))
+  }
+        
+    };
+    
+</script>
